8f2fab0c761c3e4071c1b8fcf55ab462899506cd
Matthieu Moquet <matthieu@moquet.net>
2012-08-15 15:23:44 +0200

    Initial commit

---
+++ src/Stone/Command/MirrorCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Stone\Command;
+
+use Composer\Factory;
+use Composer\IO\NullIO;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MirrorCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('mirror')
+            ->setDescription('Mirror repositories')
+            ->addArgument('file', InputArgument::REQUIRED, 'Json file to use')
+            ->addArgument('output-dir', InputArgument::REQUIRED, 'Location where to download repositories')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $filename = $input->getArgument('file');
+        $ouputDir = $input->getArgument('output-dir');
+
+        if (!file_exists($filename)) {
+            throw new \LogicException(sprintf('File %s doen\'t exist', $filename));
+        }
+
+        $io = new NullIO();
+        $composer  = Factory::create($io, $filename);
+
+        $rm = $composer->getRepositoryManager();
+        $dm = $composer->getDownloadManager();
+        $dm->setPreferSource(true);
+
+        $root = $composer->getPackage();
+        foreach ($root->getRequires() as $link) {
+            // Lookup for the last dev package
+            $package = $rm->findPackage($link->getTarget(), '9999999-dev');
+
+            $name = $package->getPrettyName();
+            $targetDir = $ouputDir.'/'.$name;
+
+            $output->writeln('<info>Downloading</info> '.$name);
+            $dm->download($package, $targetDir);
+        }
+    }
+}
