4630a55c7955a61dc0ee7a2ad8bfcb85d0927857
Matthieu Moquet <matthieu@moquet.net>
2012-08-16 23:05:19 +0200

    Code refactoring

--- src/Stone/Command/MirrorCommand.php
+++ src/Stone/Command/MirrorCommand.php
@@ -8,6 +8,7 @@ use Composer\IO\NullIO;
 use Composer\Json\JsonFile;
 use Composer\Package\Dumper\ArrayDumper;
 use Composer\Package\Loader\RootPackageLoader;
+use Composer\Package\PackageInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputArgument;
@@ -28,42 +29,27 @@ class MirrorCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $filename = $input->getArgument('file');
-        $ouputDir = $input->getArgument('output-dir');
+        $outputDir = $input->getArgument('output-dir');
 
         if (!file_exists($filename)) {
             throw new \LogicException(sprintf('File %s doen\'t exist', $filename));
         }
 
         // Create composer model
-        $io = new NullIO();
-        $composer = Factory::create($io, $filename);
-        $rm = $composer->getRepositoryManager();
-        $dm = $composer->getDownloadManager();
-        $dm->setPreferSource(true);
+        $composer = Factory::create(new NullIO(), $filename);
 
         // Retrieves installed packages
-        $installedFile = new JsonFile($ouputDir.'/installed.json');
-        $intalledPackages = $this->getInstalledPackages($composer, $installedFile);
+        $installedFile = new JsonFile($outputDir.'/installed.json');
+        $installedPackages = $this->getInstalledPackages($composer, $installedFile);
 
         // Retrieves all requires and download them
         $repositories = array();
         $packages = array();
-        foreach ($composer->getPackage()->getRequires() as $link) {
-            // Lookup for the last dev package
-            $package = $rm->findPackage($link->getTarget(), '9999999-dev');
-
+        foreach ($this->getPackagesToInstall($composer) as $package) {
             $name = $package->getPrettyName();
-            $targetDir = $ouputDir.'/'.$name;
+            $targetDir = $outputDir.'/'.$name;
 
-            if (isset($intalledPackages[$name])) {
-                // Updating
-                $output->writeln(sprintf('<info>Updating</info> <comment>%s</comment>', $name));
-                $dm->update($intalledPackages[$name], $package, $targetDir);
-            } else {
-                // Downloading
-                $output->writeln(sprintf('<info>Downloading</info> <comment>%s</comment>', $name));
-                $dm->download($package, $targetDir);
-            }
+            $this->fetchPackage($composer, $package, $output, $targetDir, $installedPackages);
 
             $packages[] = $package;
             $repositories[] = array(
@@ -72,25 +58,12 @@ class MirrorCommand extends Command
             );
         }
 
-        // Dump installed packages to installed.json
-        $dumper = new ArrayDumper();
-        $data = array();
-        foreach ($packages as $package) {
-            $data[] = $dumper->dump($package);
-        }
-        $installedFile->write($data);
+        $output->writeln('<info>Saving</info> installed repositories');
+        $this->dumpInstalledPackages($installedFile, $packages);
 
-        // Dump Satis configuration file
         $output->writeln('<info>Dumping</info> satis config to <comment>satis.json</comment>');
         $file = new JsonFile('satis.json');
-        $config = array(
-            'name' => 'Stone repositories',
-            'homepage' => 'http://packages.example.org',
-            'repositories' => $repositories,
-            'require-all' => true
-        );
-
-        $file->write($config);
+        $this->dumpSatisConfig($file, $repositories);
     }
 
     /**
@@ -114,5 +87,86 @@ class MirrorCommand extends Command
         }
 
         return $packages;
+    }
+
+    /**
+     * Get the packages to install.
+     *
+     * Returns Composer's requires
+     *
+     * @param Composer $composer Composer model
+     */
+    private function getPackagesToInstall(Composer $composer)
+    {
+        $manager = $composer->getRepositoryManager();
+        $requires = $composer->getPackage()->getRequires();
+
+        $packages = array();
+        foreach ($requires as $link) {
+            $package = $manager->findPackage($link->getTarget(), '9999999-dev');
+            $packages[] = $package;
+        }
+
+        return $packages;
+    }
+
+    /**
+     * Download or update a package.
+     *
+     * @param Composer         $composer
+     * @param PackageInterface $package
+     * @param OutputInterface  $output
+     * @param string           $targetDir
+     * @param array            $installedPackages
+     */
+    private function fetchPackage(Composer $composer, PackageInterface $package, OutputInterface $output, $targetDir, array $installedPackages = array())
+    {
+        $name = $package->getPrettyName();
+        $manager = $composer->getDownloadManager();
+        $manager->setPreferSource(true);
+
+        if (isset($installedPackages[$name])) {
+            // Updating
+            $output->writeln(sprintf('<info>Updating</info> <comment>%s</comment>', $name));
+            $manager->update($installedPackages[$name], $package, $targetDir);
+        } else {
+            // Downloading
+            $output->writeln(sprintf('<info>Downloading</info> <comment>%s</comment>', $name));
+            $manager->download($package, $targetDir);
+        }
+    }
+
+    /**
+     * Dump installed packages to installed.json.
+     *
+     * @param JsonFile $file     installed.json file
+     * @param array    $packages List of packages
+     */
+    private function dumpInstalledPackages(JsonFile $file, array $packages)
+    {
+        $dumper = new ArrayDumper();
+        $data = array();
+        foreach ($packages as $package) {
+            $data[] = $dumper->dump($package);
+        }
+        $file->write($data);
+    }
+
+    /**
+     * Dump Satis configuration file.
+     *
+     * @param JsonFile $file         The configuration file
+     * @param array    $repositories The repositories installed
+     */
+    private function dumpSatisConfig(JsonFile $file, array $repositories)
+    {
+        $config = array(
+            'name' => 'Stone repositories',
+            'homepage' => 'http://packages.example.org',
+            'repositories' => $repositories,
+            'require-all' => true
+        );
+
+        $file->write($config);
     }
 }
